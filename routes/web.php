80fda7103dd92c8c20b48f757d1a354498d267be
koi-ken <moritamitsu@outlook.jp>
2024-02-22 18:44:19 +0900

    ルーティング作成、コントローラ作成

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\ToppageController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +16,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [ToppageController::class, '__invoke']);
+Route::get('/signin', function () {
+    return view('signin');
 });
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::post('/signin_process', [UserController::class, 'signin']);
+Route::post('/login_process', [UserController::class, 'login']);
+Route::get('/logout', [UserController::class, 'logout']);
+Route::post('/loading_task', [TaskController::class, 'loading_task']);
+Route::post('/add_task', [TaskController::class, 'add_task']);
+Route::post('/delete_task',[TaskController::class, 'delete_task']);
+
+
